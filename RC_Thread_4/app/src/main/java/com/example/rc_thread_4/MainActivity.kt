9e39059d0d1186586cfe6987017af549ad88c462
Eun <43868540+JegalEun@users.noreply.github.com>
2022-02-28 15:28:35 +0900

    4주차 쓰레드 과제 시작

---
+++ RC_Thread_4/app/src/main/java/com/example/rc_thread_4/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rc_thread_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("4주차 시작","4주차 시작")
+    }
+}
