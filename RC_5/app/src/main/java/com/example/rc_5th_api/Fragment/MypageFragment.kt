aaee074bdb90935361def46f46d41f66fcd91fa5
Eun <43868540+JegalEun@users.noreply.github.com>
2022-03-08 12:41:53 +0900

    마이페이지 구현

--- RC_5/app/src/main/java/com/example/rc_5th_api/Fragment/MypageFragment.kt
+++ RC_5/app/src/main/java/com/example/rc_5th_api/Fragment/MypageFragment.kt
@@ -1,6 +1,33 @@
 package com.example.rc_5th_api.Fragment
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rc_5th_api.Adapter.ViewPagerAdapter
+import com.example.rc_5th_api.R
+import com.example.rc_5th_api.SetAddressActivity
+import com.example.rc_5th_api.databinding.FragmentHomeBinding
+import com.example.rc_5th_api.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
+
+    private lateinit var binding : FragmentMypageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+
+    }
 }
