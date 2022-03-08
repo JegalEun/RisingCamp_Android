2ce2d90782a8981536a8b0dec71e47bfb9d70741
Eun <43868540+JegalEun@users.noreply.github.com>
2022-03-08 19:38:48 +0900

    경도 위도 가져오기

--- RC_5/app/src/main/java/com/example/rc_5th_api/SetAddressActivity.kt
+++ RC_5/app/src/main/java/com/example/rc_5th_api/SetAddressActivity.kt
@@ -1,23 +1,23 @@
 package com.example.rc_5th_api
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
+import android.location.LocationRequest
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.rc_5th_api.databinding.ActivitySetAddressBinding
-import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class SetAddressActivity : AppCompatActivity(), OnMapReadyCallback {
+//class SetAddressActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySetAddressBinding
 
@@ -30,6 +30,8 @@ class SetAddressActivity : AppCompatActivity(), OnMapReadyCallback {
     val DEFAULT_ZOOM_LEVEL = 17f
     val CITY_HALL = LatLng(37.5662952, 126.97794509999994)
     var googleMap : GoogleMap? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource : CancellationTokenSource? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,38 +40,43 @@ class SetAddressActivity : AppCompatActivity(), OnMapReadyCallback {
         val view = binding.root
         setContentView(view)
 
-        setupGoogleMap()
+        initVariables()
+        // 시작하자마자 권한 요청
+        requestLocationPermissions()
+
+//        setupGoogleMap()
 
 //        binding.mapView.onCreate(savedInstanceState)
 
-        binding.flSetGps.setOnClickListener {
-            Log.d("눌림","눌림")
-            onMyLocationButtonClick()
-        }
+//        binding.flSetGps.setOnClickListener {
+//            Log.d("눌림","눌림")
+//            onMyLocationButtonClick()
+//        }
 
-        if(checkPermissions()) {
-//            initMap()
-        } else {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
-        }
+//        if(checkPermissions()) {
+////            initMap()
+//        } else {
+//            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
+//        }
 
-//        setupGoogleMap()
+        // 구글 맵
+        setupGoogleMap()
     }
 
-    private fun checkPermissions() : Boolean {
-        for(permission in PERMISSIONS) {
-            if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false
-            }
-        }
-        return true
+//    private fun checkPermissions() : Boolean {
+//        for(permission in PERMISSIONS) {
+//            if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+//                return false
+//            }
+//        }
+//        return true
+//    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-//        initMap()
-    }
 
 //    @SuppressLint("MissingPermission")
 //    fun initMap() {
@@ -92,41 +99,86 @@ class SetAddressActivity : AppCompatActivity(), OnMapReadyCallback {
 //            }
 //        }
 //    }
-
-    @SuppressLint("MissingPermission")
-    fun getMyLocation() : LatLng {
-
-        val locationProvider : String = LocationManager.GPS_PROVIDER
-        val locationNetwork : String = LocationManager.NETWORK_PROVIDER
-        val locationManager  = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val lastKnowLocation : Location? = locationManager.getLastKnownLocation(locationProvider)
-
-        if(lastKnowLocation==null) {
-            val lastKnowLocation : Location? = locationManager.getLastKnownLocation(locationNetwork)
-            val latitude = lastKnowLocation?.latitude
-            val longitude = lastKnowLocation?.longitude
-            Log.d("Test", "GPS Location changed, Latitude: $latitude" + ", Longitude: $longitude")
-            return LatLng(lastKnowLocation!!.latitude, lastKnowLocation!!.longitude)
-        }
-        return LatLng(lastKnowLocation!!.latitude, lastKnowLocation!!.longitude)
-
-    }
-
-    private fun onMyLocationButtonClick(){
-        when {
-            checkPermissions() -> googleMap?.moveCamera(
-                CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL)
-            )
-            else -> Toast.makeText(this, "위치사용권한 설정에 동의해주세요", Toast.LENGTH_SHORT).show()
-        }
-    }
-
+//
+//    @SuppressLint("MissingPermission")
+//    fun getMyLocation() : LatLng {
+//
+//        val locationProvider : String = LocationManager.GPS_PROVIDER
+//        val locationNetwork : String = LocationManager.NETWORK_PROVIDER
+//        val locationManager  = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        val lastKnowLocation : Location? = locationManager.getLastKnownLocation(locationProvider)
+//
+//        if(lastKnowLocation==null) {
+//            val lastKnowLocation : Location? = locationManager.getLastKnownLocation(locationNetwork)
+//            val latitude = lastKnowLocation?.latitude
+//            val longitude = lastKnowLocation?.longitude
+//            Log.d("Test", "GPS Location changed, Latitude: $latitude" + ", Longitude: $longitude")
+//            return LatLng(lastKnowLocation!!.latitude, lastKnowLocation!!.longitude)
+//        }
+//        return LatLng(lastKnowLocation!!.latitude, lastKnowLocation!!.longitude)
+//
+//    }
+//
+//    private fun onMyLocationButtonClick(){
+//        when {
+//            checkPermissions() -> googleMap?.moveCamera(
+//                CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL)
+//            )
+//            else -> Toast.makeText(this, "위치사용권한 설정에 동의해주세요", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
     private fun setupGoogleMap() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.frgment_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(p0: GoogleMap?) {
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if(!locationPermissionGranted){
+            finish()
+        }else {
+            cancellationTokenSource = CancellationTokenSource()
+
+            fusedLocationProviderClient.
+            getCurrentLocation(
+                com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener { location ->
+                binding.tvAddressDetail.text = "${location.latitude}, ${location.longitude}"
+            }
+        }
+//        initMap()
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+
+    private fun requestLocationPermissions(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
 
     }
 }
