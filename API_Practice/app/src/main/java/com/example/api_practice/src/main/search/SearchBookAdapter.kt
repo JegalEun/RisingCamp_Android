683ac5e0b20a2904b7e8f929e759e03eabc87992
Eun <43868540+JegalEun@users.noreply.github.com>
2022-03-16 23:27:14 +0900

    도서검색 recyclerview adapter 구현

--- API_Practice/app/src/main/java/com/example/api_practice/src/main/search/SearchBookAdapter.kt
+++ API_Practice/app/src/main/java/com/example/api_practice/src/main/search/SearchBookAdapter.kt
@@ -1,42 +1,56 @@
 package com.example.api_practice.src.main.search
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.api_practice.databinding.RvSearchItemBinding
 import com.example.api_practice.src.main.today.now.models.Book
 
-class SearchBookAdapter : ListAdapter<Book, SearchBookAdapter.BookItemViewHolder>(diffUtil) {
+//class NowRecyclerViewAdapter(private val dataList: ArrayList<BookData>) : ListAdapter<Book, NowRecyclerViewAdapter.BookItemViewHolder>(diffUtil) {
+class SearchBookAdapter(private val dataList: ArrayList<SearchBook>) : RecyclerView.Adapter<SearchBookAdapter.BookSearchViewHolder>() {
 
-    inner class BookItemViewHolder(private val binding : RvSearchItemBinding) : RecyclerView.ViewHolder(binding.root){
+    lateinit var binding: RvSearchItemBinding
 
-        fun bind(bookModel : Book) {
-            binding.tvBookTitle.text = bookModel.title
+    inner class BookSearchViewHolder(private val binding : RvSearchItemBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(data: SearchBook) {
+//            binding.tvBookTitle.text = data
+//            Glide
+//                .with(binding..context)
+//                .load(data.img)
+//                .into(binding.ivBook)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
-        return BookItemViewHolder(RvSearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookSearchViewHolder {
+        // viewholder를 생성하는 부분
+        binding = RvSearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BookSearchViewHolder(binding)
     }
 
     // 데이터를 그려주게되는 함수
-    override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
-        holder.bind(currentList[position])
+    override fun onBindViewHolder(holder: BookSearchViewHolder, position: Int) {
+        Log.d("ddddd","dddd")
+        holder.bind(dataList[position])
     }
 
-    companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<Book>() {
-            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
-                return oldItem == newItem
-            }
-
-            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-
-        }
+    override fun getItemCount(): Int {
+        return dataList.size
     }
+
+
+//    companion object {
+//        val diffUtil = object : DiffUtil.ItemCallback<Book>() {
+//            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+//                return oldItem == newItem
+//            }
+//
+//            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+//                return oldItem.id == newItem.id
+//            }
+//        }
+//    }
+
+
 }
--- API_Practice/app/src/main/java/com/example/api_practice/src/main/search/SearchFragment.kt
+++ API_Practice/app/src/main/java/com/example/api_practice/src/main/search/SearchFragment.kt
@@ -1,15 +1,39 @@
 package com.example.api_practice.src.main.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.api_practice.R
 import com.example.api_practice.config.BaseFragment
 import com.example.api_practice.databinding.FragmentSearchBinding
+import com.example.api_practice.src.main.search.models.SearchBookDto
+import com.example.api_practice.src.main.today.now.BookData
+import com.example.api_practice.src.main.today.now.NowService
+import com.example.api_practice.src.main.today.now.SearchFragmentView
+import com.example.api_practice.src.main.today.now.SearchService
 
-class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
+data class SearchBook(val title : String, val img : String)
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search), SearchFragmentView {
+
+    var dataList = ArrayList<SearchBook>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        SearchService(this).tryGetSearchBooks()
+
+    }
+
+    override fun onGetSearchBookSuccess(response: SearchBookDto) {
+        for (book in response.books) {
+            dataList.add(SearchBook(book.title, book.coverLargeUrl))
+            Log.d("BestSeller", book.title)
+            Log.d("Url",book.coverSmallUrl)
+        }
+    }
+
+    override fun onGetSearchBookFailure(message: String) {
+
     }
 
 }
